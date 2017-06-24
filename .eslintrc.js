bc49a33e6e6dcae7ff3c9279dfa1cba8564cd97b
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2017-06-24 13:42:24 +0100

    Setting up a LOT of things, adding new packages, adding ESLint and adding the project folder file structure

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  rules: {
+    'comma-dangle': 0,
+    'no-use-before-define': 0,
+    'quote-props': 1,
+    'react/jsx-no-bind': 1,
+    'react/require-default-props': 0,
+    'react/prefer-stateless-function': 1,
+    'padded-blocks': 0,
+    'no-shadow': 1,
+    'no-return-assign': 1,
+    'no-case-declarations': 1,
+    'no-underscore-dangle': 0,
+    'global-require': 0,
+    'import/no-unresolved': 1,
+    'no-param-reassign': 0,
+    'no-mixed-operators': 0,
+    'consistent-return': 1,
+    'react/no-multi-comp': 1,
+    'no-confusing-arrow': 0,
+    'react/jsx-filename-extension': 0,
+    'react/forbid-prop-types': 0,
+    'no-console': 0,
+    'no-undef': 0
+  }
+};
