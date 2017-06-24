1ca5828c1b9e9b65b98aee5982af3a1613fa48bb
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2017-06-24 15:21:41 +0100

    Wiring the navigator up with redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+
+import store from './store';
 
 import User from '../user/User';
 import Event from '../event/Event';
 import Question from '../question/Question';
 
 import './App.css';
+
 // import logo from './logo.svg';
+const history = createHistory();
 
 const App = () =>
-  (<Router>
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Main App</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
-      <ul>
-        <li><Link to="/">User</Link></li>
-        <li><Link to="/event">Event</Link></li>
-        <li><Link to="/question">Question</Link></li>
-      </ul>
+  (<Provider store={store}>
+    {/* ConnectedRouter will use the store from Provider automatically */}
+    <ConnectedRouter history={history}>
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Main App</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <ul>
+          <li><Link to="/">User</Link></li>
+          <li><Link to="/event">Event</Link></li>
+          <li><Link to="/question">Question</Link></li>
+        </ul>
 
-      <hr />
-
-      <Route path="/user" component={User} />
-      <Route path="/event" component={Event} />
-      <Route path="/question" component={Question} />
-    </div>
-  </Router>);
+        <hr />
+        <div>
+          <Route path="/user" component={User} />
+          <Route path="/event" component={Event} />
+          <Route path="/question" component={Question} />
+        </div>
+      </div>
+    </ConnectedRouter>
+  </Provider>);
 
 export default App;
