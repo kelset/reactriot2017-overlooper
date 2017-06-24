0e4916a10c33b387c561d8290d4e259d77fc1bcb
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2017-06-24 15:52:53 +0100

    setting up proptypes

--- src/event/EventList.js
+++ src/event/EventList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import EventCard from './EventCard';
 
 const EventList = ({ events }) =>
   (<div>
     {events.forEach(event => <EventCard event={event} />)}
   </div>);
+
+EventList.propTypes = {
+  events: PropTypes.array.isRequired
+};
 
 export default EventList;
